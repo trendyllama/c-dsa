c2933df3f29c4cacbd59ff1c0df79df38d31bb00
trendyllama <143849158+trendyllama@users.noreply.github.com>
2024-08-30 22:33:02 +0000

    move implementation of queue to cpp file

--- src/queue.hpp
+++ src/queue.hpp
@@ -13,111 +13,14 @@ private:
     Node<T> *front;
 
 public:
-    Queue()
-    {
-        rear = nullptr;
-        front = nullptr;
-    }
+    Queue();
+    ~Queue();
+    bool is_empty();
+    void enqueue(T data);
+    void dequeue();
+    T peek_rear();
+    T peek_front();
 
-    ~Queue()
-    {
-        while (!is_empty())
-        {
-            dequeue();
-        }
-    }
-
-    bool is_empty()
-    {
-        if (rear == nullptr && front == nullptr)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-
-    void enqueue(T data)
-    {
-
-        Node<T> *newNode = new Node<T>;
-
-        if (is_empty())
-        {
-
-            newNode->data = data;
-            newNode->next = nullptr;
-
-            rear = newNode;
-            front = newNode;
-        }
-        else
-        {
-
-            newNode->data = data;
-            rear->next = newNode;
-
-            // update rears previous value to our new node so
-            // we can eventually dequeue the last node easily
-
-            // change the address at rear to new node
-            rear = newNode;
-        }
-    }
-
-    void dequeue()
-    {
-        if (is_empty())
-        {
-
-            std::cout << "cannot dequeue when there's nothing in the queue" << std::endl;
-        }
-
-        Node<T> *temp = front;
-
-        if (rear == front)
-        {
-
-            front = rear = nullptr;
-        }
-        else
-        {
-
-            // front = nullptr;
-
-            front = front->next;
-        }
-
-        delete temp;
-    }
-
-    T peek_rear()
-    {
-        if (is_empty())
-        {
-            std::cout << "queue is empty" << std::endl;
-            return -1;
-        }
-        else
-        {
-            return rear->data;
-        }
-    }
-
-    T peek_front()
-    {
-        if (is_empty())
-        {
-            std::cout << "queue is empty" << std::endl;
-            return -1;
-        }
-        else
-        {
-            return front->data;
-        }
-    }
 };
 
 #endif
