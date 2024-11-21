e9d242a840006f2a00fffa6d1b5c75da0681306c
trendyllama <143849158+trendyllama@users.noreply.github.com>
2024-11-21 10:06:12 +0000

    refactor: move stack implementation from cpp file to header file

--- src/stack.hpp
+++ src/stack.hpp
@@ -1,8 +1,5 @@
-
+#include <iostream>
 #include "node.hpp"
-
-#ifndef STACK_H
-#define STACK_H
 
 
 template <typename T>
@@ -22,4 +19,47 @@ public:
     void peek(){}
 };
 
-#endif
+Stack<int>::Stack() {
+
+    top = nullptr;
+}
+
+
+void Stack<int>::push(int inputData) {
+
+        Node<int> *temp = new Node<int>;
+        temp->data = inputData;
+        temp->next = this->top;
+
+        if (size > max)
+        {
+            std::cout << "Stack Overflow!!";
+            exit(0);
+        }
+
+        if (temp->next == nullptr)
+        {
+            size++;
+            top = temp;
+        }
+        else
+        {
+            size++;
+            temp->next = top;
+            top = temp;
+        }
+    }
+
+void Stack<int>::pop() {
+
+        size--;
+        Node<int> *newTop = top->next;
+        delete top;
+        top = newTop;
+}
+
+void Stack<int>::peek() {
+
+    std::cout << top->data << std::endl;
+
+}
