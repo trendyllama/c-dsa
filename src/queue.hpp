e006655c5342815923f3b924a85577c35cc12a86
trendyllama <143849158+trendyllama@users.noreply.github.com>
2024-11-21 10:06:01 +0000

    refactor: move queue implementation from cpp file to header file

--- src/queue.hpp
+++ src/queue.hpp
@@ -1,8 +1,5 @@
-#include <iostream>
 #include "node.hpp"
 
-#ifndef QUEUE_H
-#define QUEUE_H
 
 template <typename T>
 class Queue
@@ -23,4 +20,118 @@ public:
 
 };
 
-#endif
+
+
+template <typename T>
+Queue<T>::Queue()
+{
+
+    rear = nullptr;
+    front = nullptr;
+}
+
+template <typename T>
+Queue<T>::~Queue()
+{
+    while (!is_empty())
+    {
+        dequeue();
+    }
+}
+
+template <typename T>
+bool Queue<T>::is_empty()
+{
+    if (rear == nullptr && front == nullptr)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+template <typename T>
+void Queue<T>::enqueue(T data)
+{
+
+    Node<T> *newNode = new Node<T>;
+
+    if (is_empty())
+    {
+
+        newNode->data = data;
+        newNode->next = nullptr;
+
+        rear = newNode;
+        front = newNode;
+    }
+    else
+    {
+
+        newNode->data = data;
+        rear->next = newNode;
+
+        // update rears previous value to our new node so
+        // we can eventually dequeue the last node easily
+
+        // change the address at rear to new node
+        rear = newNode;
+    }
+}
+
+template <typename T>
+void Queue<T>::dequeue()
+{
+    if (is_empty())
+    {
+
+        std::cout << "cannot dequeue when there's nothing in the queue" << std::endl;
+    }
+
+    Node<T> *temp = front;
+
+    if (rear == front)
+    {
+
+        front = rear = nullptr;
+    }
+    else
+    {
+
+        // front = nullptr;
+
+        front = front->next;
+    }
+
+    delete temp;
+}
+
+template <typename T>
+T Queue<T>::peek_rear()
+{
+    if (is_empty())
+    {
+        std::cout << "queue is empty" << std::endl;
+        return -1;
+    }
+    else
+    {
+        return rear->data;
+    }
+}
+
+template <typename T>
+T Queue<T>::peek_front()
+{
+    if (is_empty())
+    {
+        std::cout << "queue is empty" << std::endl;
+        return -1;
+    }
+    else
+    {
+        return front->data;
+    }
+}
